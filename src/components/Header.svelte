19443aa018493e7113d2585fe1ce31d94f6e95be
abidsulaiman <abidqubit@gmail.com>
2022-08-24 11:39:04 +0530

    header added

---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<nav>
+	<span>Logo</span>
+
+	<ul>
+		<li>Home</li>
+		<li>About</li>
+		<li>Contact</li>
+	</ul>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	h1 {
+		font-size: 4rem;
+	}
+</style>
